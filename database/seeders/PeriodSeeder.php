8950af59388f2e8d57223e9c538ee77ed64a1e74
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-02-19 02:08:40 +0700

    Update PeriodSeeder.php

--- database/seeders/PeriodSeeder.php
+++ database/seeders/PeriodSeeder.php
@@ -17,9 +17,21 @@ class PeriodSeeder extends Seeder
         $period = new Period();
         $period->name = 0;
         $period->news = 'Perkembangan kehidupan yang semakin cepat membuat manusia semakin serakah sehingga tidak mempedulikan satu dengan yang lain. Hal ini membuat penduduk E-Nation dilanda musibah kelaparan, padi gagal panen, kekeringan terjadi akibat cuaca yang ekstrem. Semua penduduk berusaha bertahan hidup, mereka harus mengeksplor pulau-pulau yang ada untuk mengumpulkan bahan-bahan makanan yang ada di beberapa ladang yang masih terdapat bahan makanan yang dapat menghasilkan makanan, di sisi lain mereka juga memerlukan alat-alat yang menunjang agar bisa cepat sampai di pulau-pulau tersebut.';
-        $period->egg = 200;
+        $period->flour = 100;
+        $period->flour_math = 1;
+        $period->egg = 100;
         $period->egg_math = 1;
-        $period->egg_change = 120;
+        $period->meat = 100;
+        $period->meat_math = 1;
+        $period->oil = 100;
+        $period->oil_math = 1;
+        $period->iron = 100;
+        $period->iron_math = 1;
+        $period->wood = 100;
+        $period->wood_math = 1;
+        $period->cloth = 100;
+        $period->cloth_math = 1;
+        $period->egg_change = 0;
         $period->save();
     }
 }
